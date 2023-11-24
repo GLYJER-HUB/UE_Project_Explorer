898f26494caaba01fcf2cd745580dd2268bc8f30
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2023-11-24 15:25:39 -0500

    add new routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import DrawerNav from "./components/Sidenav.jsx";
-
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+// import DrawerNav from "./components/Sidenav.jsx";
+import Home from "./pages/Home.jsx";
+import Informatique from "./pages/Informatique.jsx";
+import Comptabilite from "./pages/Comptabilite.jsx";
+import Gestion from "./pages/Gestion.jsx";
+import Education from "./pages/Education.jsx";
+import Layout from "./components/Layout.jsx";
 function App() {
   return (
     <>
-      <DrawerNav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="informatique" element={<Informatique />} />
+            <Route path="comptabilite" element={<Comptabilite />} />
+            <Route path="gestion" element={<Gestion />} />
+            <Route path="education" element={<Education />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
