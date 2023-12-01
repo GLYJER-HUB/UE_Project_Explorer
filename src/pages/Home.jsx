7d0b566dc3da65761b1879dbb5c216805ae184bb
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2023-12-01 11:29:25 -0500

    fetch all projects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
-import {React,useState} from "react";
+import { React, useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import DisplayGrid from "../components/DisplayGrid";
 import Loader from "../components/Loader";
 const Home = () => {
-   const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+   useEffect(() => {
+     const fetchProjects = async () => {
+       const response = await fetch("http://localhost:4000/api/projects");
+       const responseData = await response.json();
+       setProjects(responseData.projects);
+       setLoading(false);
+     };
+
+     fetchProjects();
+   }, []);
+
   return (
     <>
       <Box component="main" sx={{ flexGrow: 1, p: 5 }} alignItems="center">
@@ -17,7 +29,7 @@ const Home = () => {
         >
           Tous les projets
         </Typography>
-        {loading ? <Loader /> : <DisplayGrid/>}
+        {loading ? <Loader /> : <DisplayGrid  projectList={projects}/>}
       </Box>
     </>
   );
