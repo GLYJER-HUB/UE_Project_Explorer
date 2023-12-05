1e735f86813c2c7d5237c2d20ab42270c0976f06
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2023-12-05 13:56:45 -0500

    implement clear filter feature

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
-import { styled } from "@mui/material/styles";
 import colors from "../utilities/color";
+import { Button, styled, Paper, Stack } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "inherit",
@@ -17,20 +16,24 @@ export default function FilterButton({
   options,
   selectedOption,
   onOptionChange,
+  clearFilter,
 }) {
   return (
     <div>
       <Stack direction="row" spacing={2}>
         {Array.isArray(options)
-          ? options.map((option,index) => {
+          ? options.map((option, index) => {
               return (
                 <Item
                   key={index}
                   sx={{
-                    borderRadius: 8,
+                    borderRadius: 24,
                     background:
                       selectedOption === option ? colors.primary : "inherit",
                     color: selectedOption === option ? "white" : colors.primary,
+                    ":hover": {
+                      cursor: "pointer",
+                    },
                   }}
                   onClick={() => onOptionChange(option)}
                 >
@@ -39,6 +42,15 @@ export default function FilterButton({
               );
             })
           : ""}
+        {selectedOption && (
+          <Button
+            variant="text"
+            sx={{ color: colors.primary }}
+            onClick={clearFilter}
+          >
+            <ClearIcon />
+          </Button>
+        )}
       </Stack>
     </div>
   );
