0ef2095bb4cccca1bf912026f08dbafd0dadd11d
whis99 <hanswhisler@gmail.com>
2024-01-15 19:28:38 -0500

    searchbar mobile

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,13 +13,14 @@ const Search = styled("div")(({ theme }) => ({
     backgroundColor: colors.cardBackground,
   },
   marginLeft: 10,
+  marginRight: 10,
   width: "100%",
   [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
     width: "auto",
   },
   [theme.breakpoints.down("sm")]: {
-    width: "130%", // Full width for mobile screens
+    width: "108%", // Full width for mobile screens
     margin: "0 auto",
   },
 }));
